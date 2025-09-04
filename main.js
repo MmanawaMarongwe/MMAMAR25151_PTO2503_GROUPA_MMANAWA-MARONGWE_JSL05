3eca13f18c6a55af65191bd614d15c51315872fc
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2025-09-04 22:34:34 +0200

    Add functionality to close new modal using the x button

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ import {
   OpenEmptyModal,
   newTaskBtnDesk,
   newTaskBtnMobile,
+  closeModal,
+  MODAL_CLOSE_BTN,
 } from "./modules/NewTaskModal.js";
 
 renderTasks();
 newTaskBtnMobile.addEventListener("click", OpenEmptyModal);
 newTaskBtnDesk.addEventListener("click", OpenEmptyModal);
+
+MODAL_CLOSE_BTN.addEventListener("click", closeModal);
--- modules/NewTaskModal.js
+++ modules/NewTaskModal.js
@@ -1,7 +1,8 @@
 export const newTaskBtnMobile = document.getElementById("add-task");
 export const newTaskBtnDesk = document.getElementById("add-task-desktop");
 const MODAL = document.getElementById("empty-modal");
-const MODAL_CLOSE_BTN = document.getElementsByClassName("modal-close")[0];
+export const MODAL_CLOSE_BTN =
+  document.getElementsByClassName("modal-close")[1];
 
 const MODAL_TITLE = document.getElementById("new-task-name");
 const MODAL_DESC = document.getElementById("new-task-description");
@@ -10,4 +11,8 @@ const MODAL_STATUS = document.getElementById("new-task-status");
 export function OpenEmptyModal() {
   console.log("This is running correctly");
   MODAL.style.display = "flex";
+}
+
+export function closeModal() {
+  MODAL.style.display = "none";
 }
