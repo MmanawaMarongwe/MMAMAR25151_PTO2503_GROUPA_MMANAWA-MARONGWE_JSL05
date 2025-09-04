14f6f71316c331ebc5c60d3e4fe10b40f7895409
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2025-09-04 21:55:34 +0200

    Add module for displaying a new task module with no data

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 import { renderTasks } from "../modules/userInterface.js";
+import {
+  OpenEmptyModal,
+  newTaskBtnMobile,
+  newTaskBtnDesk,
+} from "../NewTaskModal.js";
 
 renderTasks();
+newTaskBtnMobile.addEventListener("click", OpenEmptyModal);
+newTaskBtnDesk.addEventListener("click", OpenEmptyModal);
