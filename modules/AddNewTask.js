91d714e3f57cfd5480af136a7f3ebb16f027756a
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2025-09-11 20:03:50 +0200

    Remove redundant code

--- modules/AddNewTask.js
+++ modules/AddNewTask.js
@@ -49,11 +49,6 @@ export function AddNewTask() {
     status: newTaskStatus,
   };
 
-  /* if (!newTaskTitle || !newTaskDesc || !newTaskStatus) {
-    throw new Error("All fields must be filled before adding a task");
-  } else {
-    tasks.push(newTask);
-  }*/
   tasks.push(newTask);
   localStorage.setItem("tasks", JSON.stringify(tasks));
   renderTasks();
